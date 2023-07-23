d51cd857d52af6148cb3e962c9998b170ebc9abf
Brucearmstrong007 <brucearmstrongofficial@gmail.com>
2023-07-23 20:03:45 +0530

    Angular Ionic Nest and Mongo with Docker Initialized

---
+++ server/apps/server/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
+}
+bootstrap();
