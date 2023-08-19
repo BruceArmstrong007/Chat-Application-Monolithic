80233afe75784748c8730a555e57638ef163d84a
Brucearmstrong007 <brucearmstrongofficial@gmail.com>
2023-08-19 19:51:36 +0530

    Contacts API & Data structure changes

--- server/apps/api/src/user/database/model/user.model.ts
+++ server/apps/api/src/user/database/model/user.model.ts
@@ -1,5 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Type } from '@nestjs/passport';
 import { Types, Document } from 'mongoose';
+
+@Schema({ _id: false, versionKey: false, timestamps: true })
+export class Contacts extends Document {
+  @Prop({ required: true, type: Types.ObjectId, ref: 'User' })
+  user: Types.ObjectId; // Reference to other users
+
+  @Prop({ required: true })
+  status: string;
+  
+}
+
+const ContactSchema = SchemaFactory.createForClass(Contacts);
 
 @Schema({ versionKey: false, timestamps: true })
 export class User extends Document {
@@ -14,44 +27,32 @@ export class User extends Document {
 
   @Prop({ required: true })
   password: string;
-  
+
   @Prop({ maxlength: 50 })
   name: string;
-  
+
   @Prop({ maxlength: 500 })
   bio: string;
 
   @Prop()
   profileURL: string;
 
-  @Prop({
-    type: [{ type: Types.ObjectId, ref: 'User' }],
-    validate: [
-      (contacts) => contacts.length <= 100,
-      'Limit Exceeded',
-    ],
-  })
-  contacts: Types.ObjectId[];
+  @Prop({ type: [ContactSchema], default: []  ,  validate: [
+    (contacts) => contacts.length <= 100,
+    'Limit Exceeded',
+  ], })
+  contacts: Contacts[];
 
-  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }],
-    validate: [
-      (sentInvites) => sentInvites.length <= 50,
-      'Limit Exceeded',
-    ],
-  })
-  sentInvites: Types.ObjectId[];
+  @Prop({ type: [ContactSchema], default: [] ,  validate: [
+    (sentInvites) => sentInvites.length <= 50,
+    'Limit Exceeded',
+  ], })
+  sentInvites: Contacts[];
 
-  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }],
-  validate: [
+  @Prop({ type: [ContactSchema], default: [] ,  validate: [
     (receivedInvites) => receivedInvites.length <= 50,
     'Limit Exceeded',
-  ],
- })
-  receivedInvites: Types.ObjectId[];
-
-
+  ],})
+  receivedInvites: Contacts[];
 }
-
-
 export const UserSchema = SchemaFactory.createForClass(User);
-
